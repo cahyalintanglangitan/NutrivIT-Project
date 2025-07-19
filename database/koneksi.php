818bb5c50249896f03c5929d0a7d765f851785cd
Zulpadlisyar <email>
2025-07-19 15:07:38 +0700

    update

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,12 +1,22 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "nutrivit"; // Ganti jika nama database kamu berbeda
+// --- Konfigurasi Database ---
+$servername = "127.0.0.1";
+$username   = "root";
+$password   = "";
+$dbname     = "nutrivit";
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+// --- Membuat Koneksi ---
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+// --- Memeriksa Koneksi ---
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
 }
+
+// Mengatur character set ke utf8mb4
+if (!$conn->set_charset("utf8mb4")) {
+    die("Error mengatur charset: " . $conn->error);
+}
+
+// Koneksi berhasil, siap digunakan
 ?>
